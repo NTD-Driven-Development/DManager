93d4fadc72a38c2b3d65db9df5b8cb30202ca7ad
GXiang314 <akz232032@gmail.com>
2023-08-25 14:34:06 +0800

    updated.

--- backend/src/utils/pagination.ts
+++ backend/src/utils/pagination.ts
@@ -6,13 +6,13 @@ const withPagination = async (
     offset: number = 1,
     limit: number = 20
 ) => {
-    const total = queryCount
+    const total = _.toInteger(queryCount)
     const per_page = _.toInteger(limit)
     const current_page = _.toInteger(offset)
     const last_page =
         Math.ceil(queryCount / per_page) === 0 ? 1 : Math.ceil(queryCount / per_page)
     const from = (current_page - 1) * per_page + 1 < 1 ? 0 : (current_page - 1) * per_page + 1
-    const to = offset * limit < queryCount ? offset * limit : queryCount
+    const to = current_page * per_page < queryCount ? current_page * per_page : queryCount
     // data pagination
     const items = _.slice(data, from - 1, to)
 
