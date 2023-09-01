ddf041759007690bf6c8ef60b75137387ab326ff
GXiang314 <akz232032@gmail.com>
2023-09-02 01:55:31 +0800

    updated.

--- backend/migrations/20230901215706-addColumn-contacted_at_to_tel_card_log.js
+++ backend/migrations/20230901215706-addColumn-contacted_at_to_tel_card_log.js
@@ -5,6 +5,7 @@ module.exports = {
         await queryInterface.addColumn('tel_card_log', 'contacted_at', {
             type: Sequelize.DATE,
             allowNull: false,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
         });
     },
     async down(queryInterface, Sequelize) {
