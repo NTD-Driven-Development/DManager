2550e72b86fe093fe6cd77bce73dabed4e412ec8
GXiang314 <akz232032@gmail.com>
2023-09-14 02:36:34 +0800

    updated.

--- backend/src/core/importDtos/users/UpdateUserDto.ts
+++ backend/src/core/importDtos/users/UpdateUserDto.ts
@@ -1,5 +1,7 @@
 export default class UpdateUserDto {
     public readonly id!: number
-    public readonly email?: string
+    // public readonly email?: string
     public readonly name?: string
+    public readonly sid?: string
+    public readonly role_ids?: number[]
 }
